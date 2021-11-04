0dc0bcae261f958ce7cfbfa67187b257a200c20f
claudio-q3 <84135559+claudio-q3@users.noreply.github.com>
2021-11-03 21:23:11 -0300

    feat: agregamos las rutas

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import HomePage from './components/home/HomePage';
+import FavPage from './components/favs/FavPage';
+import LoginPage from './components/login/LoginPage';  
 
 const Routes = () => {
   return (
     <Switch>
-      <Route exact path="/" component={Home} />
+      <Route exact path="/" component={HomePage} />
       <Route path="/favs" component={FavPage} />
       <Route path="/login" component={LoginPage} />
     </Switch>
